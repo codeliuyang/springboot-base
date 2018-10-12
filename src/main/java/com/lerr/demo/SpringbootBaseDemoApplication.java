f858178cfc132e3f51b90dc4e433de3ed39be91c
liuyang <codeliuyang@163.com>
2018-10-12 13:22:30 +0800

    use @Slf4j instead of logger

--- src/main/java/com/lerr/demo/SpringbootBaseDemoApplication.java
+++ src/main/java/com/lerr/demo/SpringbootBaseDemoApplication.java
@@ -1,6 +1,10 @@
 package com.lerr.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,9 +15,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @MapperScan(basePackages = "com.lerr.demo.mapper")
+@Slf4j
 public class SpringbootBaseDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootBaseDemoApplication.class, args);
+        log.info("project is running, see api doc at http://localhost:8090/demo/swagger-ui.html");
     }
 }
